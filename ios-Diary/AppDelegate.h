5e75fffdbc6bce8460d8e87de1b7e1d3d19e3ed6
mju <mju@mjuui-macbook-air-2.local>
2016-06-29 11:12:40 +0900

    Initial Commit

---
+++ ios-Diary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-Diary
+//
+//  Created by mju on 2016. 6. 29..
+//  Copyright © 2016년 recipeGo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-Diary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-Diary
+//
+//  Created by mju on 2016. 6. 29..
+//  Copyright © 2016년 recipeGo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
